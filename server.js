51244d2ea56c24026e12fc083173a1863d40ff1c
Gabriella <gabriellaomenye@gmail.com>
2026-08-10 01:06:36 +0100

    Prepare app for deployment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const { createClient } = require("@supabase/supabase-js");
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(express.static("public"));
